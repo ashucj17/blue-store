154c25e3f762218923a5211e6653003e4dac71a9
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-01-10 15:19:28 +0530

    accounts page completed

--- app.js
+++ app.js
@@ -19,3 +19,20 @@ for (let i = 0; i < smallImg.length; i++) {
     productImg.src = smallImg[i].src;
   };
 }
+
+// active form
+
+let loginForm = document.getElementById("loginForm");
+let regForm = document.getElementById("regForm");
+let indicator = document.querySelector(".indicator");
+
+function register() {
+  regForm.style.transform = "translateX(0px)";
+  loginForm.style.transform = "translateX(0px)";
+  indicator.style.transform = "translateX(100px)";
+}
+function login() {
+  regForm.style.transform = "translateX(300px)";
+  loginForm.style.transform = "translateX(300px)";
+  indicator.style.transform = "translateX(0px)";
+}
